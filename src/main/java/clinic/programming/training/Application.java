68b8b14bbe19816d44ee90ecb7270cfc2da66ceb
James Pillai <james.pillai@adopia.com>
2017-12-05 08:41:19 -0600

    Updated compiler plugin to Java 1.8

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,26 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
+	
+	public void countWords(String words){
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+		
+		for(String greet : greetings){System.out.println("Greeting: "+greet);}
+		
+	}
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	app.countWords("This is james here.");
     }
 }
