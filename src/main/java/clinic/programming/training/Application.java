e74c5d7a74e9a9da230ac3d460536777224c587b
James Pillai <james.pillai@adopia.com>
2017-12-06 11:41:19 -0600

    Committing from Eclipse

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,9 +2,20 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
+
 
 public class Application {
     
+	
+	public int countWordsInString(String words){
+		String[] seperatewords = StringUtils.split(words, ' ');
+		
+		return (seperatewords == null )? 0: seperatewords.length;
+		  
+	}
+	
     public Application() {
         System.out.println ("Inside Application");
     }
@@ -12,6 +23,7 @@ public class Application {
 	public void countWords(String words){
 		List<String> greetings = new ArrayList<>();
 		greetings.add("Hello");
+		System.out.println();
 		
 		for(String greet : greetings){System.out.println("Greeting: "+greet);}
 		
@@ -22,5 +34,8 @@ public class Application {
     	System.out.println ("Starting Application");
 	Application app = new Application();
 	app.countWords("This is james here.");
+	
+	int i= app.countWordsInString("This is james here. Hello");
+	System.out.println("Count: "+ i);
     }
 }
